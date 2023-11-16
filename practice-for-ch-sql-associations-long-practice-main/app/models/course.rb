6bb7a39f3b2764a868278e5cfc4556f54ccaeb39
wremsen <wcremsen@gmail.com>
2023-11-16 15:47:16 -0500

    fix instructor id issue

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -10,4 +10,19 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
+
+    has_many :enrolled_students,
+        through: :enrollments,
+        source: :student
+
+    belongs_to :prerequisite,
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course,
+        optional: true
+
 end

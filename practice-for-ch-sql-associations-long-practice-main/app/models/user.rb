fa1ebbd647836c41f408d4d6f282076c4bcc372f
Shaun Jhingoor <Jhingoor1945@gmail.com>
2023-11-16 15:22:31 -0500

    Start of user

--- practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
@@ -8,6 +8,15 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+    has_many :enrollments, 
+        primary_key: :id, 
+        foreign_key: :student_id,
+        class_name: :Enrollment
+
+
+    has_many :enrolled_courses, 
+        through: :enrollments,
+        source: :course
 
     
 end
